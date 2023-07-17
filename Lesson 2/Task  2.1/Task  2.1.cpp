067b8b0b4540ff63d7e432dd2761f4abf870ecd9
MaksimCPP8 <maksgreat1978@gmail.com>
2023-07-17 18:24:04 +0400

    Added Task 2.1, Task 2.2 to the Lesson 2 folder

---
+++ Lesson 2/Task  2.1/Task  2.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+long int Fib(long int number)
+{
+    if (number < 1) { return 0; }
+    if (number == 1) { return 1; }
+    return Fib(number - 1) + Fib(number - 2);
+}
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача №1. Снова числа Фибоначчи.\n";
+    long int answer;
+    std::cout << "\nВведите число: ";
+    std::cin >> answer;
+    std::cout << "\nЧисло Фибоначчи числа " << answer << " равно " << Fib(answer) << ".\n";
+
+}
---
+++ Lesson 2/Task 2.2/Task 2.2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+long int Fib_dyn(int number)
+{
+    long int *Fib = new long int[number + 1];
+    Fib[0] = 0;
+    Fib[1] = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        Fib[i] = Fib[i - 1] + Fib[i - 2];
+    }
+    return Fib[number];
+    delete[] Fib;
+}
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача №2. Ускоряем Фибоначчи.\n";
+    int answer;
+    std::cout << "\nВведите число: ";
+    std::cin >> answer;
+    std::cout << "\nЧисло Фибоначчи числа " << answer << " равно " << Fib_dyn(answer) << ".\n";
+}
